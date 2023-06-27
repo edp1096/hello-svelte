a5a2b0484342b000a145c509eb76913a6f0794cb
edp1096 <edp1096@naver.com>
2023-06-27 18:57:38 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Taste web components using svelete
+
+Source: https://www.thisdot.co/blog/web-components-with-svelte
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import Counter from './Counter.svelte';
+customElements.define('my-counter', Counter);
+
+import Clock from './Clock.svelte';
+customElements.define('my-clock', Clock);
