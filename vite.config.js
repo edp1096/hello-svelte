29001eedf469eac056fcb9f1125c6df2957fb9ae
edp1096 <edp1096@naver.com>
2023-07-01 14:08:23 +0900

    chunk

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import liveReload from 'vite-plugin-live-reload'
 import { name as pkgname } from './package.json'
@@ -6,17 +6,14 @@ import { name as pkgname } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), liveReload(['./src/**/*.svelte'], { alwaysReload: true })],
+  plugins: [svelte(), liveReload(['./src/**/*.svelte'], { alwaysReload: true }), splitVendorChunkPlugin()],
   base: "",
   build: {
     rollupOptions: {
       output: {
-        // entryFileNames: `assets/${pkgname}-[hash].js`,
-        // chunkFileNames: `assets/${pkgname}-[hash].js`,
-        // assetFileNames: `assets/${pkgname}-[hash].[ext]`,
-        entryFileNames: `assets/${pkgname}.js`,
-        chunkFileNames: `assets/${pkgname}.js`,
-        assetFileNames: `assets/${pkgname}.[ext]`,
+        entryFileNames: `assets/${pkgname}-[hash].js`,
+        assetFileNames: `assets/${pkgname}-[hash].[ext]`,
+        chunkFileNames: `assets/[name]-[hash].js`,
       },
     },
   },
